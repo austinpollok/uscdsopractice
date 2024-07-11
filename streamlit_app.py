a5e3faaf6742fb27a17be20f13bf1b4a422a7d9d
Austin Pollok <50191385+austinpollok@users.noreply.github.com>
2024-07-10 19:03:03 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.write("# Streamlit Calculator")
+
+number_1 = st.number_input("number 1")
+number_2 = st.number_input("number 2")
+number_3 = number_1 + number_2
+
+st.write("# Answer is ", number_3)
+
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.line_chart(chart_data)
