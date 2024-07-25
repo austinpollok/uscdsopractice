fc00d1ca1c7034e6b1a06103dc91b1d89911db31
Austin Pollok <50191385+austinpollok@users.noreply.github.com>
2024-07-24 20:52:53 -0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,12 +16,12 @@ chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 st.line_chart(chart_data)
 
 
-# url = 'https://drive.google.com/file/d/12GNv_tTdQBuFawJ0Jc9RW82-2qfMBmlS/view?usp=drive_link'
-# path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
-# df = pd.read_csv(path)
-# df.set_index(0, inplace=True)
-# df.sort_index(inplace=True)
+url = 'https://drive.google.com/file/d/12GNv_tTdQBuFawJ0Jc9RW82-2qfMBmlS/view?usp=drive_link'
+path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
+df = pd.read_csv(path)
+df.set_index(0, inplace=True)
+df.sort_index(inplace=True)
 
-# st.write("Hello")
+st.write(df.describe())
 
-# st.line_chart(df)
+st.line_chart(df)
